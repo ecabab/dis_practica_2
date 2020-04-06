b6c4ea5e322d351f1cfb5f03e5fbd7b357867ef2
JorgeAguirre <Jorguirre99@gmail.com>
2020-04-06 14:29:20 +0200

    Creacion de procedimientos read y write de JSON

--- src/main/java/com/ufv/practica2/agenda/Agenda.java
+++ src/main/java/com/ufv/practica2/agenda/Agenda.java
@@ -1,6 +1,13 @@
 package com.ufv.practica2.agenda;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Agenda {
 	
@@ -27,7 +34,40 @@ public class Agenda {
 		this.contactos = contactos;
 	}
 	
-	
+	public Agenda ReadJSON() {
+		 
+		Gson gson = new Gson();
+		 
+		Agenda agenda = new Agenda();
+
+	    try (Reader reader = new FileReader("agenda.json")) {
+
+	    	agenda = gson.fromJson(reader, Agenda.class);
+	        return agenda;
+
+	     } catch (IOException e) {
+	    	 
+	    	 e.printStackTrace();
+	         return agenda;
+	           
+	     }
+	        
+	}
+	public void GenerateJSON(Agenda agenda) {
+		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		
+		try (FileWriter writer = new FileWriter("agenda.json")) {
+			
+            gson.toJson(agenda, writer);
+            
+        } catch (IOException e) {
+        	
+            e.printStackTrace();
+            
+        }
+		
+	}
 	
 	
 }
